e5ccd5d39648fd9fbfd1f9cedd270fbb783f8b10
Subham Mishra <subhammishra1204@gmail.com>
2025-04-10 10:34:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+fetch('https://api.github.com/users/SubhamMishra1204/repos')
+  .then(response => response.json())
+  .then(repos => {
+    const list = document.getElementById('repo-list');
+    list.innerHTML = repos.map(repo => `<p><a href="${repo.html_url}" target="_blank">${repo.name}</a></p>`).join('');
+  });
 
